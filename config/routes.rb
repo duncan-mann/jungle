d65eb3301ccaa0f1ec937636e633042ccdbbfac6
Duncan <duncan.s.mann@gmail.com>
2019-11-14 19:04:42 -0500

    Added user-authorization

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,14 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+# these routes are for showing users a login form, logging them in, and logging them out.
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
+get '/signup' => 'users#new'
+post '/users' => 'users#create'
+
   
 
   resources :orders, only: [:create, :show]
